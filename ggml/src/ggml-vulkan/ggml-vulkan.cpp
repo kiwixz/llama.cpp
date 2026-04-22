339927c90cd7db3f8e6fa4a523bebec2dfbf5b0e
kiwixz <kiwixz@outlook.com>
2026-04-23 01:44:51 +0200

    Disable large matrix multiplications for better occupancy

--- ggml/src/ggml-vulkan/ggml-vulkan.cpp
+++ ggml/src/ggml-vulkan/ggml-vulkan.cpp
@@ -3578,6 +3578,21 @@ static void ggml_vk_load_shaders(vk_device& device) {
             } else if (!ggml_vk_matmul_int_shmem_support(device, l_intid, true, t)) {
                 device->mul_mat_id_l_int[i] = false;
             }
+
+            constexpr int ubatch = 1024;
+            constexpr int min_dispatch_size = 64;
+            if (ubatch / (m_wg_denoms[0] * m_wg_denoms[1]) < min_dispatch_size) {
+                device->mul_mat_m[i] = false;
+                device->mul_mat_m_int[i] = false;
+                device->mul_mat_id_m[i] = false;
+                device->mul_mat_id_m_int[i] = false;
+            }
+            if (ubatch / (l_wg_denoms[0] * l_wg_denoms[1]) < min_dispatch_size) {
+                device->mul_mat_l[i] = false;
+                device->mul_mat_l_int[i] = false;
+                device->mul_mat_id_l[i] = false;
+                device->mul_mat_id_l_int[i] = false;
+            }
         }
     }
 
